d2af902c792212a40e1a9f09100b40a47ce985e0
Samyak <samyakjainbvs@gmail.com>
2020-05-24 00:50:41 +0530

    Commit

--- Ifrah/settings.py
+++ Ifrah/settings.py
@@ -134,8 +134,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Ifrah/static/')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
