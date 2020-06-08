fc019b3f3887dd596f5f50bedd217f1fc401c301
Samyak <samyakjainbvs@gmail.com>
2020-06-08 22:10:31 +0530

    image

--- Gallery/models.py
+++ Gallery/models.py
@@ -27,7 +27,7 @@ class Painting(models.Model):
     width = models.IntegerField()
     sold = models.BooleanField(default=False)
     description = models.TextField()
-    image = models.ImageField(upload_to='media/',default=TempImg)
+    Image = models.ImageField(upload_to='media/',default=TempImg)
 
     def __str__(self):
         return self.title
